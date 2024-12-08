cb57abc4ef05d407f712896e9c21e88b55f3680e
m4xxh <23661067+m4xxh@users.noreply.github.com>
2024-12-08 14:21:24 +0100

    Rules class definition

--- main.py
+++ main.py
@@ -5,13 +5,35 @@ from collections.abc import Collection
 
 
 class Rules:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        distinct_objects: Collection[str],
+        ruleset: dict[tuple[str, str], str],
+    ):
+        num_objects: int = len(distinct_objects)
+        assert (num_objects - 1) * num_objects / 2 == len(
+            ruleset
+        ), "Not enough rules to cover object relations"
+        self.distinct_objects: Collection = distinct_objects
+        self.ruleset: dict[tuple[str, str], str] = ruleset
 
-
-class Player:
-    def __init__(self):
-        pass
+    @classmethod
+    def parse(cls, path: str | os.PathLike):
+        with open(path, "r") as file:
+            rules: list[str] = file.read().splitlines()
+        ruleset: list[tuple[str, str, str]] = [
+            matches
+            for line in rules
+            if len(matches := tuple(re.findall(r"(\w+)", line))) == 3
+        ]
+        ruleset_dict: dict[tuple[str, str], str] = {
+            (object1, object2): mechanism
+            for object1, mechanism, object2 in ruleset
+        }
+        distinct_objects: list[str] = list(
+            set(object for rule in ruleset for object in rule[::2])
+        )
+        return cls(distinct_objects, ruleset_dict)
 
 
 class HumanPlayer:
