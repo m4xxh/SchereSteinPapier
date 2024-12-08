3580b9665a6c10058e8923a0a4d4380ecc2e3fe6
m4xxh <23661067+m4xxh@users.noreply.github.com>
2024-12-08 13:23:34 +0100

    General structure of the Python program

---
+++ main.py
@@ -0,0 +1,37 @@
+import argparse
+import re
+import os
+from collections.abc import Collection
+
+
+class Rules:
+    def __init__(self):
+        pass
+
+
+class Player:
+    def __init__(self):
+        pass
+
+
+class HumanPlayer:
+    def __init__(self):
+        pass
+
+
+class ComputerPlayer:
+    def __init__(self):
+        pass
+
+
+class Game:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
